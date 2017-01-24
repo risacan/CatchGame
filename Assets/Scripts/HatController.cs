e536a4a700dde50325e7326658954a8333a49c90
Risa Harada <supikateriburu@gmail.com>
2017-01-24 16:53:03 +0900

    :tophat: Initial Commit!!!!!

---
+++ Assets/Scripts/HatController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HatController : MonoBehaviour {
+	public Camera cam;
+	public Rigidbody2D rb2D;
+
+	// Use this for initialization
+	void Start () {
+		rb2D = GetComponent<Rigidbody2D>();
+	    if (cam == null)	{
+		    cam = Camera.main;
+	  }
+	}
+	
+	// Update is called once per physics timestep
+	void FixedUpdate () {
+		Vector3 rawPosition = cam.ScreenToWorldPoint (Input.mousePosition);
+		Vector3 targetPosition = new Vector3 (rawPosition.x, -2.76f, 0.0f);
+		rb2D.MovePosition(targetPosition);
+	}
+}

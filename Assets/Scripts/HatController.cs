c0fbec44d99d99c48ab44ce638a4051111914695
Risa Harada <supikateriburu@gmail.com>
2017-01-24 17:58:47 +0900

    :sparkles: Remove the hardcode

--- Assets/Scripts/HatController.cs
+++ Assets/Scripts/HatController.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class HatController : MonoBehaviour {
 	public Camera cam;
 	public Rigidbody2D rb2D;
+	public Vector3 position;
 
 	// Use this for initialization
 	void Start () {
+		position = transform.position;
 		rb2D = GetComponent<Rigidbody2D>();
 	    if (cam == null)	{
 		    cam = Camera.main;
@@ -17,7 +19,7 @@ public class HatController : MonoBehaviour {
 	// Update is called once per physics timestep
 	void FixedUpdate () {
 		Vector3 rawPosition = cam.ScreenToWorldPoint (Input.mousePosition);
-		Vector3 targetPosition = new Vector3 (rawPosition.x, -2.76f, 0.0f);
+		Vector3 targetPosition = new Vector3 (rawPosition.x, position.y, 0.0f);
 		rb2D.MovePosition(targetPosition);
 	}
 }

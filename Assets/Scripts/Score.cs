389b43e205cd8974d29e4bcc6c9f3b07112b9bce
Risa Harada <supikateriburu@gmail.com>
2017-01-25 15:46:23 +0900

    :sparkles: Show the score!!

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	public Text scoreText;
+	public int ballValue;
+	private int score;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		UpdateScore();
+	}
+	
+	void UpdateScore () {
+		scoreText.text = "Score: \n" + score;
+	}
+
+	void OnTriggerEnter2D () {
+		score  += ballValue;
+		UpdateScore();
+	}
+}

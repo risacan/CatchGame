b411a30f5aa1a07aa0d32a4b792e3a0c59d9f36c
Risa Harada <supikateriburu@gmail.com>
2017-01-25 12:09:18 +0900

    :sparkles: Fall the ball when the previous one destroyed
    ボールがdestroyされたら、次のボールを落とす
    ボールを落とす場所は、カメラの横幅内でランダム

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public Camera cam;
+	public Renderer rend;
+	public GameObject ball;
+	private float maxWidth;
+
+	// Use this for initialization
+	void Start () {
+		if (cam == null) {
+			cam = Camera.main;
+		}
+		Vector3 upperCorner = new Vector3 (Screen.width, Screen.height, 0.0f);
+		Vector3 targetWidth = cam.ScreenToWorldPoint (upperCorner);
+		rend = GetComponent<Renderer>();
+//		float ballWidth = ball.rend.bounds.extents.x;
+		float ballWidth = ball.GetComponent<Renderer>().bounds.extents.x;
+		maxWidth = targetWidth.x - ballWidth;
+		StartCoroutine (Spawn ());
+	}
+	
+	// Update is called once per frame
+	IEnumerator Spawn () {
+		yield return new WaitForSeconds (2.0f);
+		while (true) {
+			Vector3 spawnPosition = new Vector3 (
+				Random.Range (-maxWidth, maxWidth), // maxWidthの間でランダム
+				transform.position.y,
+				0.0f
+			);
+			Quaternion spawnRotation = Quaternion.identity;
+			Instantiate (ball, spawnPosition, spawnRotation);
+			yield return new WaitForSeconds (Random.Range (1.0f, 2.0f));
+		}
+	}
+}
